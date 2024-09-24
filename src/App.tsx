482115185d88e2be53981bcc12f7bfd9ebca0347
OgaDavid <oluwademiladedavidoga@gmail.com>
2024-09-24 13:50:32 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  const david = 9
+
+  const test = 'test'
+
+  return (
+    <div>
+      <h1>
+        React {david} {test} App
+      </h1>
+    </div>
+  )
+}
+
+export default App
